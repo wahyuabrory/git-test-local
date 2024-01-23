7f450b1fb029767cfa0854082efce0a6c4a235cc
wahyuabrory <wahyuabrory@gmail.com>
2024-01-23 14:05:01 +0700

    Menambahkan lorem ipsum

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello, world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia magnam, voluptatibus libero error eaque non quod itaque vel numquam est explicabo nulla sunt et iste, praesentium deserunt expedita odio natus.</p>
 </body>
 </html>
